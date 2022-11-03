7929d67d2780f72633ad88cba7245f165fbb37b4
milenko31 <milenko.satara@gmail.com>
2022-11-03 14:17:11 +0100

    last commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+// import About from '/pages/About.js'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <link to="/"> Accueil</link>
+            </li>
+            <li>
+              <link to="/A_propos"> A propos</link>
+            </li>
+          </ul>
+        </nav>
+      </Router>
+      <Switch>
+        {/*<Route path="/" exact component={Home} />*/}
+        {/*<Route path="/A_propos" exact component={About} />*/}
+        <Route path="*"> Not Found </Route>
+      </Switch>
     </div>
   );
 }
