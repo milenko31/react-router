5454c931d4dd7767d7f4183bbd394e9bd756c530
milenko31 <milenko.satara@gmail.com>
2022-11-05 12:41:58 +0100

    commit 05/11/2022

--- src/Pages/HomePage/Homepage.js
+++ src/Pages/HomePage/Homepage.js
@@ -3,7 +3,17 @@ import React from 'react'
 export default function Homepage() {
   return (
     <div>
-      <p>lorem </p>
+      <div className="picture_and_text">
+        <img
+          className="heading_picture"
+          src="picture_header.png"
+          alt="image vers le header"
+        />
+        <p className="text_on_picture">Chez vous, partout et ailleurs</p>
+      </div>
+      <section className="big_card">
+        
+      </section>
     </div>
   )
 }
